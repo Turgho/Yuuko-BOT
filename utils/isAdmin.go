477978fdab75118f84c16093709cb55c3f9364d2
Turgho <victor.hugo3692111@gmail.com>
2025-05-15 15:08:46 -0300

    feat: adicionar função para verificação da role admin

---
+++ utils/isAdmin.go
@@ -0,0 +1,21 @@
+package utils
+
+import "github.com/bwmarrin/discordgo"
+
+func IsAdmin(s *discordgo.Session, guildID, userID string) bool {
+	member, err := s.GuildMember(guildID, userID)
+	if err != nil {
+		return false
+	}
+
+	for _, roleID := range member.Roles {
+		role, err := s.State.Role(guildID, roleID)
+		if err != nil {
+			continue
+		}
+		if role.Permissions&discordgo.PermissionAdministrator != 0 {
+			return true
+		}
+	}
+	return false
+}
