7bcf61c7cf030e0c835a244eccd3e5d7d716c580
Turgho <victor.hugo3692111@gmail.com>
2025-05-15 18:54:12 -0300

    feat: adicionar novo comando para desligar o bot

---
+++ commands/admin/shutdown.go
@@ -0,0 +1,34 @@
+package admin
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func ShutdownSlashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// Responde à interação avisando que vai desligar
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "🛑 Desligando o bot...",
+			Flags:   discordgo.MessageFlagsEphemeral, // mensagem visível só para quem executou
+		},
+	})
+	if err != nil {
+		// Se não conseguir responder, tenta enviar mensagem no canal
+		s.ChannelMessageSend(i.ChannelID, "A mimir, patrão... 😴")
+	}
+
+	log.Println("Bot desligado às ", time.Now().Format(time.RFC3339))
+
+	// Fecha a sessão para desconectar do gateway
+	s.Close()
+
+	time.Sleep(3 * time.Second)
+
+	// Finaliza o processo
+	os.Exit(0)
+}
