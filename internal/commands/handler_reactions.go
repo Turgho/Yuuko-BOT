3858b6480bd515bbcc9f1df89e14b1565b35ba01
Turgho <victor.hugo3692111@gmail.com>
2025-08-27 18:30:50 -0300

    refator: modificação para as reações de cada guilda

--- internal/commands/handler_reactions.go
+++ internal/commands/handler_reactions.go
@@ -2,54 +2,34 @@ package commands
 
 import (
 	"log"
-	"os"
-	"sync"
+
+	"Turgho/Yuuko-BOT/config"
+	"Turgho/Yuuko-BOT/internal/services/logger"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
-)
-
-var (
-	loadEnvOnce    sync.Once
-	roleID         string
-	rulesMessageID string
 )
 
 // HandleReactionAdd é chamado quando um usuário adiciona uma reação em uma mensagem.
-// Caso seja a mensagem de regras com o emoji ✅, o usuário recebe o cargo de membro.
 func HandleReactionAdd(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
-	// Carrega .env apenas uma vez
-	loadEnvOnce.Do(func() {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Erro ao carregar .env")
-		}
-
-		roleID = os.Getenv("CARGO_MEMBRO")
-		if roleID == "" {
-			log.Fatal("CARGO_MEMBRO não definido no .env")
-		}
-
-		rulesMessageID = os.Getenv("RULES_MESSAGE_ID")
-		if roleID == "" {
-			log.Fatal("RULES_MESSAGE_ID não definido no .env")
-		}
-	})
-
 	// Ignora reações do próprio bot
 	if r.UserID == s.State.User.ID {
 		return
 	}
 
-	// Verifica se a reação é na mensagem correta com o emoji correto
-	if r.MessageID == rulesMessageID && r.Emoji.Name == "✅" {
-		err := s.GuildMemberRoleAdd(r.GuildID, r.UserID, roleID)
-		if err != nil {
-			log.Printf("❌ Erro ao adicionar cargo ao usuário %s: %v", r.UserID, err)
+	for _, guildCfg := range config.CfgMap {
+		if r.MessageID == guildCfg.RulesMessageID && r.Emoji.Name == "✅" {
+			logger.LogReaction(s, r) // log automático
+			err := s.GuildMemberRoleAdd(r.GuildID, r.UserID, guildCfg.RoleMemberID)
+			if err != nil {
+				log.Printf("❌ Erro ao adicionar cargo ao usuário %s na guild %s: %v", r.UserID, r.GuildID, err)
+				return
+			}
+
+			_, err = s.ChannelMessageSend(r.ChannelID, "<@"+r.UserID+"> leu e aceitou as regras! ✅")
+			if err != nil {
+				log.Printf("❌ Erro ao enviar mensagem de confirmação: %v", err)
+			}
 			return
 		}
-
-		// Envia confirmação pública no canal
-		s.ChannelMessageSend(r.ChannelID, "<@"+r.UserID+"> leu e aceitou as regras! ✅")
 	}
 }
