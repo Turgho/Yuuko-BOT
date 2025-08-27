5c6abaf55d991440d24d595a30a69d2eab2121d0
Turgho <victor.hugo3692111@gmail.com>
2025-08-27 18:34:03 -0300

    chore: pequenas alterações nos métodos dos comandos

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,17 +10,20 @@ import (
 
 var PublicCommands = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 	// comandos public slash
-	"ping":  public.PingCommand,
-	"hello": public.HelloSlashCommand,
+	"ping":    public.PingCommand,
+	"hello":   public.HelloSlashCommand,
+	"weather": public.WeatherSlashCommand,
 }
 
 var AdminCommands = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 	// comandos admin slash
-	"rules":    admin.RulesSlashCommand,
-	"purge":    admin.PurgeSlashCommand,
-	"restart":  admin.RestartSlashCommand,
-	"shutdown": admin.ShutdownSlashCommand,
-	"kick":     admin.KickUserSlashCommand,
+	"rules":      admin.RulesSlashCommand,
+	"purge":      admin.PurgeSlashCommand,
+	"restart":    admin.RestartSlashCommand,
+	"shutdown":   admin.ShutdownSlashCommand,
+	"kick":       admin.KickUserSlashCommand,
+	"setmember":  admin.SetMemberRoleSlashCommand,
+	"setwelcome": admin.SetWelcomeChannelSlashCommand,
 }
 
 var GamesCommands = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
